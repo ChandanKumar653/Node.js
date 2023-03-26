7ecb8e1008c9843fd4483ef6ca32a1f3960c857e
ChandanKumar653 <chandankumar6204995@gmail.com>
2023-03-26 20:52:58 +0530

    code

--- data.js
+++
@@ -1,5 +0,0 @@
-const data = [
-    { name: 'chandan kumar', email: 'chandan@gmail.com' },
-    { name: 'lol', email: 'lol@gmail.com', contact: '4444' }
-]
-module.exports = data
--- hello.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    x: 10,
-    y: 11,
-    z: function() {
-        return 10;
-    }
-}
