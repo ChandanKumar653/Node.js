95b4c0479ae7d160620988bcc686f7adcb09d007
CHANDAN KUMAR <chandankumar6204995@gmail.com>
2023-03-26 08:23:19 +0530

    Node.js Practice Codes

---
+++ data.js
@@ -0,0 +1,5 @@
+const data = [
+    { name: 'chandan kumar', email: 'chandan@gmail.com' },
+    { name: 'lol', email: 'lol@gmail.com', contact: '4444' }
+]
+module.exports = data
---
+++ hello.js
@@ -0,0 +1,7 @@
+module.exports = {
+    x: 10,
+    y: 11,
+    z: function() {
+        return 10;
+    }
+}
